a867c8d5321f06744544aa63567abff4f36e64bc
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-03 11:30:32 -0500

    creating App function and app.css, using map to iterate through the object IDs in array

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import '../style/app.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
-    );
-  }
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div className='app'>
+//         <h1>DevCamp React Starter</h1>
+//         <h2>React Redux Router</h2>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  const [appState, changeState] = useState({
+    activeObject: null,
+    objects: [{ id:1 }, { id:2 }, { id:3 }, { id:4 }]
+  })
+  return (
+    <div className="App">
+      {appState.objects.map((elements, index) => (
+        <div key={index} className='box inactive' onClick={}></div>
+      ))}
+    </div>
+  )
 }
+
+export default App;
